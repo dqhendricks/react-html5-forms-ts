3b018de368bff6040de3ad241fa3f41a68f6de33
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2024-09-07 09:27:07 -0700

    updated to use pseudo classes

--- src/forms/useControlValidation.ts
+++ src/forms/useControlValidation.ts
@@ -3,7 +3,6 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
 export function useControlValidation<
   T extends HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
 >(onChange?: (event: React.ChangeEvent<T>) => void) {
-  const [isValid, setIsValid] = useState(true);
   const [validationMessage, setValidationMessage] = useState("");
   const controlRef = useRef<T>(null);
 
@@ -11,7 +10,6 @@ export function useControlValidation<
   const handleChange = useCallback(
     (event: React.ChangeEvent<T>) => {
       const inputElement = event.target;
-      setIsValid(inputElement.checkValidity());
       setValidationMessage(inputElement.validationMessage);
       if (onChange) onChange(event);
     },
@@ -28,7 +26,6 @@ export function useControlValidation<
   }, [handleChange]);
 
   return {
-    isValid,
     validationMessage,
     controlRef,
     handleChange,
