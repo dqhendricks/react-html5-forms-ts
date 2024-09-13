62ba942e799a02c955abb781629f85ecf67e6f0d
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2024-09-13 11:21:05 -0700

    Allows display of validation indicators after touch

--- src/forms/useControlValidation.ts
+++ src/forms/useControlValidation.ts
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 
+import styles from "./Form.module.css";
+
 export function useControlValidation<
   T extends HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
 >(onChange?: (event: React.ChangeEvent<T>) => void) {
   const [validationMessage, setValidationMessage] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
   const controlRef = useRef<T>(null);
 
-  // applys styles for displaying valid/invalid indicators
+  // sets validation message
   const handleChange = useCallback(
     (event: React.ChangeEvent<T>) => {
       const inputElement = event.target;
@@ -15,6 +18,19 @@ export function useControlValidation<
     },
     [onChange]
   );
+
+  // sets isTouched which determines whether to show validation indicators
+  const handleBlur = useCallback((event: React.FocusEvent<T>) => {
+    // skip onBlur validation if submitting
+    const { relatedTarget } = event;
+    if (relatedTarget && relatedTarget.tagName === "BUTTON") {
+      const relatedTargetType = relatedTarget.getAttribute("type");
+      if (relatedTargetType === "submit" || relatedTargetType === null) {
+        return;
+      }
+    }
+    setIsTouched(true);
+  }, []);
 
   // ensures correct validation status applied even if the user hits submit before changing values
   useEffect(() => {
@@ -25,9 +41,15 @@ export function useControlValidation<
     }
   }, [handleChange]);
 
+  const validationClasses = `${isTouched ? styles.isTouched : ""} ${
+    validationMessage ? styles.invalid : styles.valid
+  }`;
+
   return {
     validationMessage,
-    controlRef,
-    handleChange,
+    validationClasses,
+    ref: controlRef,
+    onChange: handleChange,
+    onBlur: handleBlur,
   };
 }
