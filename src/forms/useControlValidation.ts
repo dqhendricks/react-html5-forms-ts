91710fbbf31035e401983434f538be045c6353d8
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2024-09-22 16:43:45 -0700

    fixed dependency issue

--- src/forms/useControlValidation.ts
+++ src/forms/useControlValidation.ts
@@ -9,36 +9,46 @@ export function useControlValidation<
   const [isTouched, setIsTouched] = useState(false);
 
   // updates validation message on type
-  const handleChange = useCallback((event: React.ChangeEvent<T>) => {
-    const inputElement = event.target;
-    setValidationMessage(inputElement.validationMessage);
-    if (attributes.onChange) attributes.onChange(event);
-  }, []);
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<T>) => {
+      const inputElement = event.target;
+      setValidationMessage(inputElement.validationMessage);
+      if (attributes.onChange) attributes.onChange(event);
+    },
+    [attributes]
+  );
 
   // sets isTouched which determines whether to show validation indicators
-  const handleBlur = useCallback((event: React.FocusEvent<T>) => {
-    // skip onBlur validation if submitting
-    const { target: inputElement, relatedTarget } = event;
-    if (relatedTarget && relatedTarget.tagName === "BUTTON") {
-      const relatedTargetType = relatedTarget.getAttribute("type");
-      const form = relatedTarget.closest("form");
-      const buttonWillSubmit =
-        form && (relatedTargetType === "submit" || relatedTargetType === null);
-      if (buttonWillSubmit) {
-        return;
+  const handleBlur = useCallback(
+    (event: React.FocusEvent<T>) => {
+      // skip onBlur validation if submitting
+      const { target: inputElement, relatedTarget } = event;
+      if (relatedTarget && relatedTarget.tagName === "BUTTON") {
+        const relatedTargetType = relatedTarget.getAttribute("type");
+        const form = relatedTarget.closest("form");
+        const buttonWillSubmit =
+          form &&
+          (relatedTargetType === "submit" || relatedTargetType === null);
+        if (buttonWillSubmit) {
+          return;
+        }
       }
-    }
-    setValidationMessage(inputElement.validationMessage);
-    setIsTouched(true);
-    if (attributes.onBlur) attributes.onBlur(event);
-  }, []);
+      setValidationMessage(inputElement.validationMessage);
+      setIsTouched(true);
+      if (attributes.onBlur) attributes.onBlur(event);
+    },
+    [attributes]
+  );
 
   // for when user submits without clicking any of the inputs
-  const handleInvalid = useCallback((event: React.InvalidEvent<T>) => {
-    const inputElement = event.target;
-    setValidationMessage(inputElement.validationMessage);
-    if (attributes.onInvalid) attributes.onInvalid(event);
-  }, []);
+  const handleInvalid = useCallback(
+    (event: React.InvalidEvent<T>) => {
+      const inputElement = event.target;
+      setValidationMessage(inputElement.validationMessage);
+      if (attributes.onInvalid) attributes.onInvalid(event);
+    },
+    [attributes]
+  );
 
   const validationClasses = `${isTouched ? styles.isTouched : ""} ${
     validationMessage ? styles.invalid : styles.valid
